81d2d1c8f187186a08a5caffe7e40a40d5e392c1
Graciekan21 Nemusesu <graciekan21@gmail.com>
2024-03-13 10:49:26 +0000

    Prepare our environment and settings.py file, run migrations and tidy up errors

--- codestar/settings.py
+++ codestar/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+
+if os.path.isfile("env.py"):
+   import env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +26,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=watmo7_=y2=4=2l$ngujozx+h&$y_1vgx-mf(ml&6j(g3um3q'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-graciekan21-djangowande-fotzpjegwgv.ws-eu108.gitpod.io']
+ALLOWED_HOSTS = ['8000-graciekan21-djangowande-fotzpjegwgv.ws-eu109.gitpod.io']
 
 
 # Application definition
@@ -74,12 +81,10 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
 
 
 # Password validation
