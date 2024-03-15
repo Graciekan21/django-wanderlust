f3b1baef9fafc8986215a35b02883866f8e4aa04
Graciekan21 Nemusesu <graciekan21@gmail.com>
2024-03-15 13:59:51 +0000

    Create comment models

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.urls import reverse
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
@@ -23,3 +24,36 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('home')
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+    def number_of_comments(self):
+        return self.comments.count()
+
+    def category_name(self):
+        return self.category
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                            related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+class Meta:
+    ordering = ['created_on']
+
+def __str__(self):
+    return f"coment {self.boby} by {self.name}"
+
+
+
