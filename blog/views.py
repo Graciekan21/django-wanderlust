f6374dc833db0cc8db7e6a2459747e3a4deb9967
Graciekan21 Nemusesu <graciekan21@gmail.com>
2024-03-16 15:34:45 +0000

    Create the view code

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404, reverse
+from django.views import generic
+from .models import post
+  
+    
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.flilter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
