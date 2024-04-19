abf9b8092fad6d7aa8bc4d3b04d222205fe4970b
Graciekan21 Nemusesu <graciekan21@gmail.com>
2024-04-19 15:27:23 +0000

    Errors fixed in all files

--- blog/models.py
+++ blog/models.py
@@ -9,8 +9,9 @@ STATUS = ((0, "Draft"), (1, "Published"))
 class Category(models.Model):
     name_1 = models.CharField(max_length=200, unique=True)
 
-    class Meta:
-        ordering = ["-name_1"]
+
+class Meta:
+    ordering = ["-name_1"]
 
 
 categories = Category.objects.all().values_list("name_1", "name_1")
@@ -20,19 +21,19 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
-    )
+        User, on_delete=models.CASCADE, related_name="blog_posts")
     featured_image = CloudinaryField("image", default="placeholder")
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=1)
-   likes = models.ManyToManyField(User, 
-       related_name="blogpost_like", blank=True)    
+    likes = models.ManyToManyField(
+       User, related_name="blogpost_like", blank=True)
     category = models.TextField(choices=categories, default=1)
 
-    class Meta:
-        ordering = ["-created_on"]
+
+class Meta:
+    ordering = ["-created_on"]
 
     def __str__(self):
         return self.title
@@ -54,7 +55,8 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(
+       Post, on_delete=models.CASCADE, related_name="comments")
     name = models.CharField(max_length=80)
     username = models.CharField(max_length=80, default="admin")
     body = models.TextField()
